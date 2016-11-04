826fd578b2bc209e2767ce73188be155373115fd
newms34 <newms3450@gmail.com>
2016-11-04 17:51:41 -0400

    swash plate calcs done! yay trig!

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ var app = angular.module('heli', []).controller('helicon', function($scope, $com
     $scope.currEngSpeed = 0; //out of 200;
     $scope.rotX = 0;
     $scope.rotZ = 0;
-    $scope.curBladeRot = 88;
+    $scope.tiltFB = 0;
+    $scope.tiltRL = 0;
+    $scope.curBladeRot = 0;
     $scope.cylRez = 15; //global (sorta) attribute that specifies cylinder resolution: how many vertical segments per cylinder
     $scope.cylMaker = function(h, w, p, t, o, e, c) {
         //height, width, parent(selector), translation, rotation (orientation), capped(boolean)
@@ -67,18 +69,18 @@ var app = angular.module('heli', []).controller('helicon', function($scope, $com
         }
     };
     $scope.cylMaker(240, 7, '#main-shaft', { x: 0, y: 0, z: 0 }, { x: 0, y: 0, z: 0 }, false);
-    $scope.cylMaker(7, 60, '#upper-swash', { x: -5, y: 180, z: 1 }, { x: 0, y: 0, z: 0 }, true);
-    $scope.cylMaker(15, 80, '#rotor-hub', { x: -7, y: 110, z: 2 }, { x: 0, y: 0, z: 0 }, true,{h:0,s:0,v:10});
-    $scope.cylMaker(7, 60, '#lower-swash', { x: 0, y: 0, z: 0 }, { x: 0, y: 0, z: 0 }, true);
-    $scope.cylMaker(55, 3, '#c-r-f', { x: 31, y: -79, z: 0 }, { x: 0, y: 0, z: 0 }, false,{h:0,s:0,v:10});
-    $scope.cylMaker(55, 3, '#c-r-b', { x: -19, y: -79, z: 0 }, { x: 0, y: 0, z: 0 }, false,{h:0,s:0,v:10});
-    $scope.cylMaker(55, 3, '#c-r-l', { x: 6, y: -79, z: 25 }, { x: 0, y: 0, z: 0 }, false,{h:0,s:0,v:10});
-    $scope.cylMaker(55, 3, '#c-r-r', { x: 6, y: -79, z: -25 }, { x: 0, y: 0, z: 0 }, false,{h:0,s:0,v:10});
+    $scope.cylMaker(7, 60, '#upper-swash', { x: -6, y: 180, z: 0 }, { x: 0, y: 0, z: 0 }, true);
+    $scope.cylMaker(25, 80, '#rotor-hub', { x: -7, y: 105, z: 2 }, { x: 0, y: 0, z: 0 }, true, { h: 0, s: 0, v: 10 });
+    $scope.cylMaker(7, 60, '#lower-swash', { x: -6, y: 0, z: 0 }, { x: 0, y: 0, z: 0 }, true);
+    $scope.cylMaker(65, 3, '#c-r-f', { x: 31, y: -6, z: 0 }, { x: 0, y: 0, z: 0 }, false, { h: 0, s: 0, v: 10 });
+    $scope.cylMaker(65, 3, '#c-r-b', { x: -19, y: -6, z: 0 }, { x: 0, y: 0, z: 0 }, false, { h: 0, s: 0, v: 10 });
+    $scope.cylMaker(65, 3, '#c-r-l', { x: 6, y: -6, z: 25 }, { x: 0, y: 0, z: 0 }, false, { h: 0, s: 0, v: 10 });
+    $scope.cylMaker(65, 3, '#c-r-r', { x: 6, y: -6, z: -25 }, { x: 0, y: 0, z: 0 }, false, { h: 0, s: 0, v: 10 });
 
-    $scope.rectMaker(25, 1200, '#m-b-f', { x: 0, y: 0, z: 0 }, { x: 0, y: 0, z: 0 }, 'linear-gradient(to left, yellow, yellow 2%, black 2%)', true);
-    $scope.rectMaker(25, 1200, '#m-b-b', { x: 0, y: 0, z: 0 }, { x: 0, y: 0, z: 0 }, 'linear-gradient(to left, yellow, yellow 2%, black 2%)', true)
-    $scope.rectMaker(25, 1200, '#m-b-r', { x: 0, y: 0, z: 0 }, { x: 0, y: 0, z: 0 }, 'linear-gradient(to left, yellow, yellow 2%, black 2%)', true)
-    $scope.rectMaker(25, 1200, '#m-b-l', { x: 0, y: 0, z: 0 }, { x: 0, y: 0, z: 0 }, 'linear-gradient(to left, yellow, yellow 2%, black 2%)', true)
+    $scope.rectMaker(25, 1200, '#m-b-f', { x: 0, y: -5, z: 0 }, { x: 0, y: 0, z: 0 }, 'linear-gradient(to left, yellow, yellow 2%, black 2%)', true);
+    $scope.rectMaker(25, 1200, '#m-b-b', { x: 0, y: -5, z: 0 }, { x: 0, y: 0, z: 0 }, 'linear-gradient(to left, yellow, yellow 2%, black 2%)', true)
+    $scope.rectMaker(25, 1200, '#m-b-r', { x: 0, y: -5, z: 0 }, { x: 0, y: 0, z: 0 }, 'linear-gradient(to left, yellow, yellow 2%, black 2%)', true)
+    $scope.rectMaker(25, 1200, '#m-b-l', { x: 0, y: -5, z: 0 }, { x: 0, y: 0, z: 0 }, 'linear-gradient(to left, yellow, yellow 2%, black 2%)', true)
     $scope.rotsOn = true;
     window.onmousemove = function(e) {
         if ($scope.rotsOn) {
@@ -87,7 +89,7 @@ var app = angular.module('heli', []).controller('helicon', function($scope, $com
             $scope.$apply();
         }
     };
-    $scope.engOn = true;
+    $scope.engOn = false;
     window.onkeyup = function(e) {
         if (e.which == 83) {
             $scope.rotsOn = !$scope.rotsOn;
@@ -97,14 +99,37 @@ var app = angular.module('heli', []).controller('helicon', function($scope, $com
                 $scope.spinny();
             }
         } else if (e.which == 81 && $scope.currEngSpeed < 195) {
-            $scope.currEngSpeed += 5;
+            $scope.currEngSpeed += 2;
         } else if (e.which == 90 && $scope.currEngSpeed > 5) {
-            $scope.currEngSpeed -= 5;
+            $scope.currEngSpeed -= 2;
+        } else if (e.which == 88 && $scope.curBladeRot > 62) {
+            $scope.curBladeRot -= 2
+        } else if (e.which == 87 && $scope.curBladeRot < 90) {
+            $scope.curBladeRot += 2
+        } else if (e.which == 73 && $scope.tiltFB < 15) {
+            e.preventDefault();
+            $scope.tiltFB++;
+        } else if (e.which == 75 && $scope.tiltFB > -15) {
+            e.preventDefault();
+            $scope.tiltFB--;
+        } else if (e.which == 74 && $scope.tiltRL < 15) {
+            e.preventDefault();
+            $scope.tiltRL++;
+        } else if (e.which == 76 && $scope.tiltRL > -15) {
+            e.preventDefault();
+            $scope.tiltRL--;
         }
+    }
+    $scope.getFBAngle = function() {
+        return ($scope.tiltFB * Math.cos($scope.shaftCurRot * Math.PI / 180)) - ($scope.tiltRL * Math.sin($scope.shaftCurRot * Math.PI / 180));
+    }
+
+    $scope.getRLAngle = function() {
+        // console.log('GETRLANGLE: raw tiltFB',$scope.tiltFB,'raw tiltRL',$scope.tiltRL,'cos',Math.cos(($scope.shaftCurRot) * Math.PI / 180),'sin',Math.sin(($scope.shaftCurRot) * Math.PI / 180),'calced tilt fb',($scope.tiltFB * Math.cos($scope.shaftCurRot * Math.PI / 180)) + ($scope.tiltRL * Math.sin($scope.shaftCurRot * Math.PI / 180)),'calced tilt rl',($scope.tiltFB * Math.sin(($scope.shaftCurRot) * Math.PI / 180)) + ($scope.tiltRL * Math.cos(($scope.shaftCurRot) * Math.PI / 180)),'rot',$scope.shaftCurRot%360)
+        return ($scope.tiltFB * Math.sin(($scope.shaftCurRot) * Math.PI / 180)) + ($scope.tiltRL * Math.cos(($scope.shaftCurRot) * Math.PI / 180));
     }
     $scope.spinny = function() {
         setTimeout(function() {
-            console.log('WHIRRR')
             $scope.shaftCurRot += $scope.currEngSpeed;
             $scope.$apply();
             if ($scope.engOn) {
@@ -112,4 +137,25 @@ var app = angular.module('heli', []).controller('helicon', function($scope, $com
             }
         }, 50)
     };
+    $scope.getRodHeight = function(n) {
+        //heh, rod
+        n = parseInt(n);
+        var ang = 0 - 6 - Math.cos(($scope.shaftCurRot + n) * Math.PI / 180) * (40 * Math.sin(($scope.tiltFB) * Math.PI / 180));
+        ang += 0 - 6 - Math.cos(($scope.shaftCurRot + n + 90) * Math.PI / 180) * (40 * Math.sin(($scope.tiltRL) * Math.PI / 180));
+        return ang;
+    };
+    $scope.getBladeAng = function(n) {
+        var ang = Math.cos(($scope.shaftCurRot + n) * Math.PI / 180) * (40 * Math.sin(($scope.tiltFB) * Math.PI / 180));
+        ang += Math.cos(($scope.shaftCurRot + n + 90) * Math.PI / 180) * (40 * Math.sin(($scope.tiltRL) * Math.PI / 180));
+        //ang is now displacement of parent rod. now we need to convert this to a percentage of max rot
+        var maxDisp = 2 * Math.max(Math.abs((40 * Math.sin(($scope.tiltRL) * Math.PI / 180))), Math.abs((40 * Math.sin(($scope.tiltFB) * Math.PI / 180))));
+        ang += maxDisp / 2;
+        if (n == 0) {
+            console.log(ang,$scope.curBladeRot,ang,maxDisp)
+        }
+        if (isNaN(90 - ($scope.curBladeRot * ang / maxDisp))) {
+            return 90;
+        }
+        return 90 - ($scope.curBladeRot * ang / maxDisp);
+    }
 })
